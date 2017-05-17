a49df899967ce5c23abf314177d0e75d911f1658
Gravit Gravit <gravit.min@ya.ru>
2017-05-17 19:33:43 +0700

    kvadrat.cpp The derivation of a handful of an infinite matrix of squares sinus.cpp The plot of cos (x)

---
+++ kvadrat.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+const char null_char = ' ';
+const char hline_char = '_';
+const char vline_char = '|';
+
+typedef char matrix_type;
+matrix_type* matrix;
+int mPosX=0,mPosY=0; //Размеры матрицы
+double zPosX=0,zPosY=0; //Выбранная позиция
+const int size_kvadrat = 10;
+bool setPixel(double x,double y,matrix_type t)
+{
+    if(zPosX > x || ( zPosX + mPosX) < x) return false;
+    if(zPosY > y || ( zPosY + mPosY) < y) return false;
+    int real_matrix_index = round((y-zPosY)*mPosX)+round(x-zPosX);
+    matrix[real_matrix_index] = t;
+    return true;
+}
+void renderKvadrat(int x,int y,int size)
+{
+    for(int i=x;i<(x+size);i++)
+    {
+        setPixel(i+zPosX,y+zPosY,hline_char);
+    }
+    for(int i=y;i<(y+size);i++)
+    {
+        setPixel(x+zPosX,i+zPosY,vline_char);
+    }
+    for(int i=y;i<(y+size);i++)
+    {
+        setPixel(x+zPosX+size,i+zPosY,vline_char);
+    }
+    for(int i=x;i<(x+size);i++)
+    {
+        setPixel(i+zPosX,y+zPosY+size,hline_char);
+    }
+}
+int main()
+{
+    
+    //Создание
+    std::cout << "Размер матрицы(x y): ";
+    std::cin >> mPosX >> mPosY;
+    std::cout << "Текущая позиция(x y): ";
+    std::cin >> zPosX >> zPosY;
+    matrix = new matrix_type[(mPosX+1)*(mPosY+1)];
+    //Заполнение
+    for(int i=0;i<mPosY;i++)
+    for(int j=0;j<mPosX;j++)
+    {
+        matrix[i*mPosX+j] = null_char;
+    }
+    //Рисуем
+    
+    //Первая точка
+    int fristX=0,fristY=0; 
+    fristX=( (int)round(zPosX) ) % size_kvadrat;//Смещение по X
+    fristY=( (int)round(zPosY) ) % size_kvadrat;//Смещение по Y
+    for(int i=0;i<mPosX+size_kvadrat+1;i=i+size_kvadrat)
+    {
+        for(int j=0;j<mPosY+size_kvadrat+1;j=j+size_kvadrat)
+        renderKvadrat(i-fristX,j-fristY,size_kvadrat);
+    }
+    //Вывод
+    for(int i=0;i<mPosY;i++) {
+    for(int j=0;j<mPosX;j++)
+    {
+        std::cout << matrix[i*mPosX+j];
+    }
+    std::cout << std::endl;
+    }
+    delete[] matrix;
+    return 0;
+}
---
+++ sinus.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+double X_EXPANDING = 4.0;
+int posX=0,posY=0;
+int func(double arg)
+{
+    double sinArg =  ((double)arg / (double)(posX)) * (M_PI * 2.0) * X_EXPANDING;
+    double tmp = (cos(sinArg) + 1.0) / 2.0;
+    double tmp2 = (1 - tmp)*((double)posY -1);
+    return round(tmp2);
+}
+typedef unsigned char matrix_type;
+matrix_type* matrix;
+void drawLine(int x1,int y1,int x2,int y2,char t)
+{
+    int side1 = x1-x2;
+    int side2 = y1-y2;
+    
+    if(side2 == 0)
+    {
+        for(int i=x1;i>=x2;i--) matrix[y1*posX+i] = t;
+        return;
+    }
+    if(side1 == 0)
+    {
+        for(int i=y1;i>y2;i--) matrix[i*posX+x1] = t;
+        return;
+    }
+    double tgA = (double)side1 /(double)side2;
+    for(int i=x1;i>x2;i--)
+    {
+        
+        int a = y1+round(((double)i / tgA ) * posY);
+        std::cout << tgA << ' ' << i << ' ';
+        matrix[a*posX+i] = t;
+    }
+}
+int main()
+{
+    std::cout << "Размер матрицы: ";
+    std::cin >> posX >> posY;
+    std::cout << "Число проходов: ";
+    std::cin >> X_EXPANDING;
+    //Создание матрицы
+    matrix = new matrix_type[(posX+1)*(posY+1)];
+    //Заполнение
+    for(int i=0;i<posY;i++)
+    for(int j=0;j<posX;j++)
+    {
+        matrix[i*posX+j] = ' ';
+    }
+    //Построение
+    drawLine(posX,posY/2,0,posY/2 ,'-');
+    drawLine(posX/2,posY,posX/2,0 ,'|');
+    for(int i=0;i<posX;i++)
+    {
+        matrix[func(i)*posX+i] = 'x';
+        //std::cout << tmp << "/" << tmp2 << " ";
+    }
+    std::cout << "\n";
+    //Вывод
+    for(int i=0;i<posY;i++) {
+    for(int j=0;j<posX;j++)
+    {
+        std::cout << matrix[i*posX+j];
+    }
+    std::cout << "\n";
+    }
+    delete[] matrix;
+    return 0;
+}
